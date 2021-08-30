32d928ba698e6bd65c1cb0d29f0b3f77a1f020ed
vinodnk92 <89781897+vinodnk92@users.noreply.github.com>
2021-08-30 17:52:19 +0530

    Create TestRunner.java

---
+++ src/TestRunner/TestRunner.java
@@ -0,0 +1,44 @@
+package TestRunner;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.CucumberFeatureWrapper;
+import cucumber.api.testng.TestNGCucumberRunner;
+import cucumber.api.testng.*;
+
+@CucumberOptions (
+        features = "./features/"
+        ,glue = {"StepDefinition"}
+        ,tags = {"@Automation"}
+        ,monochrome = true)
+public class TestRunner {
+	public static WebDriver driver;
+    private TestNGCucumberRunner testNGCucumberRunner;
+    
+    @BeforeClass(alwaysRun = true)
+    public void setUpClass() throws Exception {    	
+        testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
+    }
+
+    @Test(dataProvider = "features")    
+    public void feature(PickleEventWrapper eventwrapper,CucumberFeatureWrapper cucumberFeature) throws Throwable {
+    	testNGCucumberRunner.runScenario(eventwrapper.getPickleEvent());
+    }
+    
+    @DataProvider//(parallel=true)
+    public Object[][] features() {
+       // return testNGCucumberRunner.provideFeatures();    	
+    	 return testNGCucumberRunner.provideScenarios();
+    }
+    
+    @AfterClass(alwaysRun = true)
+    public void tearDownClass() throws Exception {    	
+        testNGCucumberRunner.finish();  
+        
+    }
+}
